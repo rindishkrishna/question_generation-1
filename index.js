7bc34a8308d5cc297bc35d862350171cbed9ca33
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-01-01 22:12:51 +0530

    changes

--- index.js
+++ index.js
@@ -1,80 +1,3 @@
-// document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-//     const dropZoneElement = inputElement.closest(".addimg");
-
-//     dropZoneElement.addEventListener("click", (e) => {
-//         inputElement.click();
-//     });
-
-//     inputElement.addEventListener("change", (e) => {
-//         if (inputElement.files.length) {
-//             updateThumbnail(dropZoneElement, inputElement.files[0]);
-//         }
-//     });
-
-//     dropZoneElement.addEventListener("dragover", (e) => {
-//         e.preventDefault();
-//         dropZoneElement.classList.add("drop-zone--over");
-//     });
-
-//     ["dragleave", "dragend"].forEach((type) => {
-//         dropZoneElement.addEventListener(type, (e) => {
-//             dropZoneElement.classList.remove("drop-zone--over");
-//         });
-//     });
-
-//     dropZoneElement.addEventListener("drop", (e) => {
-//         e.preventDefault();
-
-//         if (e.dataTransfer.files.length) {
-//             inputElement.files = e.dataTransfer.files;
-//             updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-//         }
-
-//         dropZoneElement.classList.remove("drop-zone--over");
-//     });
-// });
-
-// /**
-//  * Updates the thumbnail on a drop zone element.
-//  *
-//  * @param {HTMLElement} dropZoneElement
-//  * @param {File} file
-//  */
-// function updateThumbnail(dropZoneElement, file) {
-//     let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-//     // First time - remove the prompt
-//     if (dropZoneElement.querySelector(".upload")) {
-//         dropZoneElement.querySelector(".upload").remove();
-//     }
-
-//     // First time - there is no thumbnail element, so lets create it
-//     if (!thumbnailElement) {
-//         thumbnailElement = document.createElement("div");
-//         thumbnailElement.classList.add("drop-zone__thumb");
-//         dropZoneElement.appendChild(thumbnailElement);
-//     }
-
-//     thumbnailElement.dataset.label = file.name;
-
-//     // Show thumbnail for image files
-
-
-
-//     if (file.type.startsWith("image/")) {
-//         const reader = new FileReader();
-
-//         reader.readAsDataURL(file);
-//         reader.onload = () => {
-//             thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-//         };
-//     } else {
-//         thumbnailElement.style.backgroundImage = null;
-//     }
-// }
-
-
-
 let input = document.querySelector('#file');
 let dropzone = document.querySelector('.drop');
 let nameBox = document.querySelector('.drop span');
@@ -106,7 +29,7 @@ let loadArrows = document.querySelector('.arrows');
 let checkmark = document.querySelector('.checkmark');
 
 uploadBtn.addEventListener('click', function () {
-    if (nameBox.innerHTML !== '' && uploadBtn.innerHTML !== 'Done') {
+    if (nameBox.innerHTML !== '' && uploadBtn.innerHTML !== '') {
         this.innerHTML = 'Uploading...'
         loadBar.classList.add('active');
 
@@ -119,7 +42,9 @@ uploadBtn.addEventListener('click', function () {
         setTimeout(function () {
             loadArrows.classList.add('hidden');
             checkmark.classList.remove('hidden');
-            uploadBtn.innerHTML = 'Done';
+            uploadBtn.innerHTML = '';
+            uploadBtn.remove()
+
         }, 3000);
     }
 });
