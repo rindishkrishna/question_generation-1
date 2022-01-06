d1883b03dba7cce303de8f02fee49f7be198a8bf
rindish krishna <rindishkrishna@gmail.com>
2022-01-06 23:09:21 +0530

    File upload api integrated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let dropzone = document.querySelector('.drop');
 let nameBox = document.querySelector('.drop span');
 let bt=document.getElementById("upload")
 bt.style.visibility="hidden";
-let url=''
+let file=''
 
 input.addEventListener('dragover', function () {
     dropzone.classList.add('active');
@@ -15,20 +15,15 @@ input.addEventListener('dragleave', function () {
 
 
 input.addEventListener('change', function () {
-    var inp = document.getElementById("file");
-var fReader = new FileReader();
-fReader.readAsDataURL(inp.files[0]);
-fReader.onloadend = function(event){
-    console.log(event.target.result);
-    // var img = document.getElementById("yourImgTag");
-    // img.src = event.target.result;
-}
+    if (!input.files) {
+        return null;
+      }
+      file = input.files[0];
     bt.style.visibility="visible";
     dropzone.classList.remove('active');
 
     //Take the file name and show it
     fileName = input.value;
-    // console.log(fileName);
     
     let index = fileName.lastIndexOf('\\');
     fileName = fileName.substring(fileName.length, index + 1);
@@ -46,24 +41,20 @@ let checkmark = document.querySelector('.checkmark');
 uploadBtn.addEventListener('click', function () {
    
     if (nameBox.innerHTML !== '' && uploadBtn.innerHTML !== '') {
-        let url=document.getElementById("file").files[0].path;
-        console.log(url);
         this.innerHTML = 'Uploading...'
         loadBar.classList.add('active');
-        
         Tesseract.recognize(
-            url,
+            file,
             'eng',
             { logger: m => console.log(m) }
           ).then(({ data: { text } }) => {
             document.getElementById("paragraph").value = text;
-              console.log(text)
             //Hide dropzone and show loading arrows
             dropzone.classList.add('hidden');
             loadArrows.classList.remove('hidden');
             loadArrows.classList.add('active');
     
-            //After 3 seconds show checkmark
+            //After 1 second show checkmark
             setTimeout(function () {
                 loadArrows.classList.add('hidden');
                 checkmark.classList.remove('hidden');
